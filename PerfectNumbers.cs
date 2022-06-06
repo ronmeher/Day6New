bcbc0adbcf15797e40ceff200e1d23d8554cd884
ronmeher <ronmeher@gmail.com>
2022-06-06 18:43:06 +0530

    Perfect Number Program added!

---
+++ PerfectNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6
+{
+    internal class PerfectNumbers
+    {
+        public void run()
+        {
+
+            int number, sum = 0, n;
+            Console.WriteLine("Enter the Number:");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+            }
+
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+            }
+
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,14 @@ namespace Day6
         {
             Console.WriteLine("Day 6 Prpblems!\n");
 
-            Console.WriteLine("Fibonacci Series!\n");
-            Fibonacci p1 = new Fibonacci();
-            p1.run();
+            //Console.WriteLine("Fibonacci Series!\n");
+            //Fibonacci p1 = new Fibonacci();
+            //p1.run();
+
+            Console.WriteLine("Perfect Numbers\n");
+            PerfectNumbers p2 = new PerfectNumbers();
+            p2.run();
+           
         }
     }
 }
